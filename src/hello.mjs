edf6a481495360577556bc374c04860dcf96a85a
zhoukekestar <zhoukekestar@163.com>
2024-11-24 17:02:36 +0800

    update

--- src/hello.mjs
+++ src/hello.mjs
@@ -1,13 +1,12 @@
+import { getEnv } from './env.mjs'
 export default async () => {
-  let message = `<h1>hello</h1>`;
+  let message = `<h1>hello</h1>`
+  const env = getEnv()
 
-  if (env['gmail-worker']) {
-    message += ' gmail-worker: ' + await env['gmail-worker'].get('test1')
-}
-if (env['gmail-worker-kv']) {
-      message += ' gmail-worker-kv: ' + await env['gmail-worker'].get('test1')
+  if (env['gmail-worker-kv']) {
+    message += ' gmail-worker-kv: ' + (await env['gmail-worker'].get('test1'))
   }
-  
+
   return new Response(message, {
     headers: { 'Content-type': 'text/html' },
     status: 200
--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,7 @@
 import { Router } from 'itty-router'
 import hello from './hello.mjs'
 import notfound from './notfound.mjs'
+import { setEnv } from './env.mjs'
 
 const router = Router()
 
@@ -18,23 +19,7 @@ router.all('*', notfound)
 
 export default {
   async fetch (request, env, ctx) {
-    const { cf } = request
-    const { city, country } = cf
-
-    console.log(env);
-
-    console.log(`Request came from city: ${city} in country: ${country}`)
-
-    let message = 'Hello worker!'
-    if (env['gmail-worker']) {
-      message += ' gmail-worker: ' + (await env['gmail-worker'].get('test1'))
-    }
-    if (env['gmail-worker-kv']) {
-      message += ' gmail-worker-kv: ' + (await env['gmail-worker-kv'].get('test1'))
-    }
-
-    return new Response(message, {
-      headers: { 'content-type': 'text/plain' }
-    })
+    setEnv(env);
+    return router.handle(request);
   }
 }
