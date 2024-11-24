985cce8c1d4ef4051ce0df130094e9f5950de7c3
zhoukekestar <zhoukekestar@163.com>
2024-11-24 22:20:41 +0800

    update

--- src/gmailInboxWebhook.mjs
+++ src/gmailInboxWebhook.mjs
@@ -1,8 +1,8 @@
 export default async ({ req, env }) => {
-    const url = new URL(req.url);
-    const body = await req.text();
+  console.log('push webhook start!')
 
-    console.log('push webhook. search: ', url.searchParams.toString(), ' body: ', body)
-    return new Response("OK")
-  }
-  
+  const body = await req.text()
+
+  console.log('push webhook. search: ', req.url, ' body: ', body)
+  return new Response('OK')
+}
