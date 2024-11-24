46c3cbf3a18edfcdbb6ce4849c051717869ba6a9
zhoukekestar <zhoukekestar@163.com>
2024-11-24 16:44:46 +0800

    update

--- src/index.mjs
+++ src/index.mjs
@@ -12,6 +12,27 @@ router.get("/hello", hello)
 
 router.all('*', notfound);
 
+// export default {
+// 	fetch: router.handle
+// }
+
 export default {
-	fetch: router.handle
-}
+    async fetch(request) {
+            const { cf } = request;
+            const { city, country } = cf;
+
+            console.log(`Request came from city: ${city} in country: ${country}`);
+
+            let message = 'Hello worker!';
+            if (env['gmail-worker']) {
+                message += ' gmail-worker: ' + await env['gmail-worker'].get('test1')
+            }
+            if (env['gmail-worker-kv']) {
+                  message += ' gmail-worker-kv: ' + await env['gmail-worker'].get('test1')
+              }
+
+            return new Response(message, {
+                    headers: { "content-type": "text/plain" },
+            });
+    },
+};
