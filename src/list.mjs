0ab02eb9e05ecb0c5f8965362d27086ca22078f1
zhoukekestar <zhoukekestar@163.com>
2024-11-24 20:58:32 +0800

    update

--- src/list.mjs
+++ src/list.mjs
@@ -14,8 +14,7 @@ export default async ({ req, env }) => {
     'x-goog-api-client': 'gdcl/7.2.0 gl-node/18.18.2',
     'Accept-Encoding': 'gzip',
     'User-Agent': 'google-api-nodejs-client/7.2.0 (gzip)',
-    Authorization:
-      credentials?.access_token || credentials?.credentials?.access_token
+    Authorization: `Bearer ${credentials?.access_token}`
   }
 
   const res = await fetch(
