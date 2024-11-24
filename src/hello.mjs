e3e473ff6bdedeaa0d3790803cc26301d21fca97
zhoukekestar <zhoukekestar@163.com>
2024-11-24 16:40:57 +0800

    update

--- src/hello.mjs
+++ src/hello.mjs
@@ -1,5 +1,13 @@
-export default () => {
-  const message = `<h1>hello</h1>`
+export default async () => {
+  let message = `<h1>hello</h1>`;
+
+  if (env['gmail-worker']) {
+    message += ' gmail-worker: ' + await env['gmail-worker'].get('test1')
+}
+if (env['gmail-worker-kv']) {
+      message += ' gmail-worker-kv: ' + await env['gmail-worker'].get('test1')
+  }
+  
   return new Response(message, {
     headers: { 'Content-type': 'text/html' },
     status: 200
