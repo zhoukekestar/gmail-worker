e2cfc5644a82f51e8eac786fd89cd0ef669ade07
keke <zhoukekestar@163.com>
2024-11-26 20:37:24 +0800

    Update auth.mjs

--- src/auth.mjs
+++ src/auth.mjs
@@ -10,8 +10,6 @@ export default async ({ env }) => {
     access_type: 'offline',
     scope: [
       'https://mail.google.com/',
-      'https://www.googleapis.com/auth/gmail.metadata',
-      'https://www.googleapis.com/auth/gmail.modify',
       'https://www.googleapis.com/auth/gmail.readonly',
       'https://www.googleapis.com/auth/gmail.compose',
       'https://www.googleapis.com/auth/gmail.send'
