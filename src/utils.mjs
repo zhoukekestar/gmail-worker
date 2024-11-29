11e97a7e47f79f5160074c8bcc483d2b2e5bb260
zhoukekestar <zhoukekestar@163.com>
2024-11-29 21:44:02 +0800

    update

--- src/utils.mjs
+++ src/utils.mjs
@@ -10,16 +10,18 @@ export const getToken = async env => {
     return credentials?.access_token
   }
 
-  return await refreshAndGetToken(env)
+  return await refreshAndGetToken(kv, credentials)
 }
 
-async function refreshAndGetToken (env) {
+async function refreshAndGetToken (kv, credentials) {
   // 初始化 authclient
-  const kv = env['gmail-worker-kv']
   const clientId = await kv.get('clientId')
   const clientSecret = await kv.get('clientSecret')
-  const credentials = JSON.parse(await kv.get('credentials'))
   const refreshToken = credentials.refresh_token
+
+  if (!refreshToken) {
+    throw new Error('no refreshtoken found!')
+  }
 
   // 构造参数
   const values = {
@@ -43,8 +45,14 @@ async function refreshAndGetToken (env) {
       tokens.refresh_token = refreshToken
     }
 
+    // 过期时间，提前一分钟刷新，避免以为没过期，但过期的情况
+    token.expired = Date.now() + token.expires_in * 1000 - 1000 * 60;
+    token.expired_iso_time = new Date(token.expired).toISOString();
+    
     // 写入更新时间
-    tokens.updated = new Date().toISOString()
+    tokens.updated_iso_time = new Date().toISOString()
+
+    // 更新
     await kv.put('credentials', JSON.stringify(tokens))
     return tokens.access_token
   }
